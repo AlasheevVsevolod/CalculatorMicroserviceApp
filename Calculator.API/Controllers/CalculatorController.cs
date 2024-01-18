98880e88ed00d9f7158c4dd6658490cb24c8c91d
Vsevolod <2007juggernaut@gmail.com>
2024-01-18 23:50:52 +0500

    Add solution and update readme

---
+++ Calculator.API/Controllers/CalculatorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.API.Controllers;
+
+[ApiController]
+[Route("api/{controller}")]
+public class CalculatorController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetTest()
+    {
+        return Ok("123");
+    }
+}
